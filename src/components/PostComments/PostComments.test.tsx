6fccb197d06eac2d09bb0083816463c23660c892
palloma g <90066965+pallomaog@users.noreply.github.com>
2023-11-23 14:09:21 -0300

    Update PostComments.test.tsx

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -7,4 +7,24 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    it('Deve adicionar dois comentários', () => {
+        render(<PostComment/>);
+
+        fireEvent.change(screen.getByTestId('comment-textarea'), {
+            target: {
+                value: 'Comentário adicionado via testes',
+            }
+        });
+        fireEvent.click(screen.getByTestId('comment-button'));
+
+        fireEvent.change(screen.getByTestId('comment-textarea'), {
+            target: {
+                value: 'Segundo comentário adicionado via testes',
+            }
+        });
+        fireEvent.click(screen.getByTestId('comment-button'));
+
+        expect(screen.getAllByTestId('comment-element')).toHaveLength(2);
+    });
 });
