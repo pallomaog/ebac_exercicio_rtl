3e1943b3a70e179897461e39687c98123c4efcdf
palloma g <90066965+pallomaog@users.noreply.github.com>
2023-11-24 09:42:35 -0300

    Update PostComments.test.tsx

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -2,24 +2,25 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import Post from '.';
 import PostComment from '.';
 
+
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
 
-    
-
     it('Deve adicionar dois comentários', () => {
         render(<PostComment/>);
 
+        // adiciona o primeiro comentário
         fireEvent.change(screen.getByTestId('comment-textarea'), {
             target: {
                 value: 'Comentário adicionado via testes',
             }
         });
         fireEvent.click(screen.getByTestId('comment-button'));
-
+    
+        // adiciona o segundo comentário
         fireEvent.change(screen.getByTestId('comment-textarea'), {
             target: {
                 value: 'Segundo comentário adicionado via testes',
@@ -29,6 +30,4 @@ describe('Teste para o componente PostComment', () => {
 
         expect(screen.getAllByTestId('comment-element')).toHaveLength(2);
     });
-
-    
 });
